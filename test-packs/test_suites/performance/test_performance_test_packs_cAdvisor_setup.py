9b7d7d9cf1e79e53752931a64ce0310b1f86b538
fengs7 <sherry.feng@dell.com>
2017-04-20 15:58:47 -0500

    Update test_performance_test_packs_cAdvisor_setup.py

--- test-packs/test_suites/performance/test_performance_test_packs_cAdvisor_setup.py
+++ test-packs/test_suites/performance/test_performance_test_packs_cAdvisor_setup.py
@@ -59,31 +59,31 @@ def test_cAdvisor_Setup():
 
     file_text_1 = 'docker run --volume=/:/rootfs:ro  \\ \n'
 
-    file_text_1 = file_text_1 + '         --volume=/var/run:/var/run:rw  \\ \n'
+    file_text_1 = file_text_1 + '           --volume=/var/run:/var/run:rw  \\ \n'
 
-    file_text_1 = file_text_1 + '         --volume=/sys:/sys:ro  \\ \n'
+    file_text_1 = file_text_1 + '           --volume=/sys:/sys:ro  \\ \n'
 
-    file_text_1 = file_text_1 + '         --volume=/var/lib/docker/:/var/lib/docker:ro  \\ \n'
+    file_text_1 = file_text_1 + '           --volume=/var/lib/docker/:/var/lib/docker:ro  \\ \n'
 
-    file_text_1 = file_text_1 + '         --publish=9820:9820  \\ \n'
+    file_text_1 = file_text_1 + '           --publish=9820:9820  \\ \n'
 
-    file_text_1 = file_text_1 + '         --detach=true  \\ \n'
+    file_text_1 = file_text_1 + '           --detach=true  \\ \n'
 
-    file_text_1 = file_text_1 + '         --name=cadvisor google/cadvisor:latest  \\ \n'
+    file_text_1 = file_text_1 + '           --name=cadvisor google/cadvisor:latest  \\ \n'
 
-    file_text_1 = file_text_1 + '           -port=9820 \\ \n'
+    file_text_1 = file_text_1 + '             -port=9820 \\ \n'
 
-    file_text_1 = file_text_1 + '           -storage_driver=influxdb  \\ \n'
+    file_text_1 = file_text_1 + '             -storage_driver=influxdb  \\ \n'
 
-    file_text_1 = file_text_1 + '           -storage_driver_db=cadvisor  \\ \n'
+    file_text_1 = file_text_1 + '             -storage_driver_db=cadvisor  \\ \n'
 
-    file_text_1 = file_text_1 + '           -storage_driver_user=root \\ \n'
+    file_text_1 = file_text_1 + '             -storage_driver_user=root \\ \n'
 
-    file_text_1 = file_text_1 + '           -storage_driver_password=root \\ \n'
+    file_text_1 = file_text_1 + '             -storage_driver_password=root \\ \n'
 
-    file_text_1 = file_text_1 + '           -storage_driver_host=' + perf_hostname + ':8086'
+    file_text_1 = file_text_1 + '             -storage_driver_host=' + perf_hostname + ':8086'
 
-
+    file_text_1 = file_text_1.encode('ascii')
     # copy cadvisor docker compose file to symphyony ova vm
 
     ssh = paramiko.SSHClient()
