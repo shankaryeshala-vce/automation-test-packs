30c7b5cf8229ead35308764118853b4b841b5161
cullia <aoife.cullinane@vce.com>
2017-05-16 13:13:44 +0100

    Updating the Node Expansion System Definition Function

--- test-packs/test_suites/continuous-integration-deploy-suite/node-discovery-ci-cd/test_a_ne_systemAddRequest_CI_CD.py
+++ test-packs/test_suites/continuous-integration-deploy-suite/node-discovery-ci-cd/test_a_ne_systemAddRequest_CI_CD.py
@@ -28,10 +28,6 @@ def load_test_data():
     payload_header = 'payload'
     global payload_property_sys
     payload_property_sys = 'sys_payload_node_exp'
-    global payload_property_req
-    payload_property_req = 'sys_request_payload'
-    global payload_property_req_config
-    payload_property_req_config = 'sys_request_payload_with_config'
 
     global ipaddress
     ipaddress = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS', property='hostname')
@@ -54,7 +50,7 @@ def load_test_data():
 #######################################################################################################################
 
 
-#@pytest.mark.dne_paqx_parent_mvp
+# @pytest.mark.dne_paqx_parent_mvp
 def test_DNE_SystemAdditionRequested():
     print('Defining System on system: ', ipaddress)
 
@@ -80,15 +76,43 @@ def test_DNE_SystemAdditionRequested():
                                              '__TypeId__': 'com.dell.cpsd.syds.converged.system.addition.requested'},
                                          payload=the_payload)
 
-    # Give the system time to be fully configured
-    time.sleep(2)
+    # #****************************************************
+    # #TODO I think This is where we can monitor the vcenter registering with consul. Connented out until code committed. Need to validate manually via trace log first.
+    #
+    # #Make sure the queue is clean
+    # af_support_tools.rmq_purge_queue(host=ipaddress, port=port, rmq_username=rmq_username, rmq_password=rmq_password,
+    #                                  queue='test.endpoint.registration.event')
+    #
+    # waitForMsg('test.controlplane.vcenter.response')
+    # return_message = af_support_tools.rmq_consume_message(host=ipaddress, port=port, rmq_username=rmq_username,
+    #                                                       rmq_password=rmq_password,
+    #                                                       queue='test.controlplane.vcenter.response',
+    #                                                       remove_message=True)
+    #
+    # checkForErrors(return_message)
+    # return_json = json.loads(return_message, encoding='utf-8')
+    # assert return_json['responseInfo']['message'] == 'SUCCESS', 'ERROR: Vcenter validation failure'
+    #
+    #
+    # waitForMsg('test.endpoint.registration.event')
+    # return_message = af_support_tools.rmq_consume_message(host=ipaddress, port=port, rmq_username=rmq_username,
+    #                                                       rmq_password=rmq_password,
+    #                                                       queue='test.endpoint.registration.event',
+    #                                                       remove_message=True)
+    #
+    # checkForErrors(return_message)
+    # return_json = json.loads(return_message, encoding='utf-8')
+    # # assert return_json['endpoint']['type'] == 'vcenter', 'vcenter not registered with endpoint'
+    # # TODO above is commented out as its consumming the wrong message
+    #
+    #
+    # #****************************************************
+
 
     # Call the function to verify the system exists. This will return the system UUID.
-    # Get the payload data from the config symphony-sds.ini file.
-    the_payload = af_support_tools.get_config_file_property(config_file=payload_file, heading=payload_header,
-                                                            property=payload_property_req)
 
-    time.sleep(1)
+    the_payload = '{"messageProperties":{"timestamp":"2010-01-01T12:00:00Z","correlationId":"sys-def-req-abcd-abcdabcdabcd","replyTo":"sds.localhost"},"systemsFilter":{"systemUuid":"","systemIdentifier":"","serialNumber":""}}'
+
     af_support_tools.rmq_publish_message(host=ipaddress,
                                          port=port, rmq_username=rmq_username, rmq_password=rmq_password,
                                          exchange='exchange.dell.cpsd.syds.system.definition.request',
@@ -122,6 +146,12 @@ def cleanup():
     af_support_tools.rmq_delete_queue(host=ipaddress, port=port, rmq_username=rmq_username, rmq_password=rmq_password,
                                       queue='test.system.list.found')
 
+    af_support_tools.rmq_delete_queue(host=ipaddress, port=port, rmq_username=rmq_username, rmq_password=rmq_password,
+                                      queue='test.controlplane.vcenter.response')
+
+    af_support_tools.rmq_delete_queue(host=ipaddress, port=port, rmq_username=rmq_username, rmq_password=rmq_password,
+                                      queue='test.endpoint.registration.event')
+
 
 def bindQueus():
     print('\nCreating Queues')
@@ -132,7 +162,17 @@ def bindQueus():
                                     exchange='exchange.dell.cpsd.syds.system.definition.response',
                                     routing_key='#')
 
-    time.sleep(2)
+    af_support_tools.rmq_bind_queue(host=ipaddress,
+                                    port=port, rmq_username=rmq_username, rmq_password=rmq_password,
+                                    queue='test.controlplane.vcenter.response',
+                                    exchange='exchange.cpsd.controlplane.vcenter.response',
+                                    routing_key='#')
+
+    af_support_tools.rmq_bind_queue(host=ipaddress,
+                                    port=port, rmq_username=rmq_username, rmq_password=rmq_password,
+                                    queue='test.endpoint.registration.event',
+                                    exchange='exchange.dell.cpsd.endpoint.registration.event',
+                                    routing_key='#')
 
 
 def waitForMsg(queue):
@@ -154,3 +194,10 @@ def waitForMsg(queue):
 
 
             #######################################################################################################################
+
+
+def checkForErrors(return_message):
+    checklist = 'errors'
+    if checklist in return_message:
+        print('\nBUG: Error in Response Message\n')
+        assert False  # This assert is to fail the test
