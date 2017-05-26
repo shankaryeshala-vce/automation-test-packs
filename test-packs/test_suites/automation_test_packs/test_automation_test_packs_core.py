cf174b3909da7dd788fdcd3f50a3839304e9e0db
Mike Maliska <michael.maliska@vce.com>
2017-05-26 10:26:24 -0400

    Cleaning up samples

--- test-packs/test_suites/automation_test_packs/test_automation_test_packs_core.py
+++ test-packs/test_suites/automation_test_packs/test_automation_test_packs_core.py
@@ -1,90 +1,26 @@
 import af_support_tools
 import os
 import pytest
+import selenium
 
 @pytest.fixture(scope="module", autouse=True)
 def load_test_data():
     # Update config ini files at runtime
     # This can be used to update ini files with sensitive data such as passwords an IP addresses
-    
-    # This updates the env.ini file with cpsd common properties
-    my_data_file = os.environ.get('AF_RESOURCES_PATH') + '/cpsd-common/cpsd_common.properties'
-    af_support_tools.set_config_file_property_by_data_file(my_data_file)
-    # This updates the sample_config.ini file with sensitive data
     my_data_file = os.environ.get('AF_RESOURCES_PATH') + '/data_sensitive_vars.properties'
     af_support_tools.set_config_file_property_by_data_file(my_data_file)
 
     # Set config ini file name
     # These can be set inside the individual tests and do not need to be made global
     global config_file
-    config_file = 'sample_config.ini'
-    global env_file
-    env_file = 'env.ini'
+    config_file = 'automation_test_packs/core_config.ini'
 
     # Set Vars
     # These can be read inside the individual tests and do not need to be made global
-
-    # CPSD Common Properties
-    global cpsd_hostname
-    cpsd_hostname = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS', property='hostname')
-    global cpsd_username
-    cpsd_username = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS', property='username')
-    global cpsd_password
-    cpsd_password = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS', property='password')
-
-    global rmq_username
-    rmq_username = af_support_tools.get_config_file_property(config_file=env_file, heading='RabbitMQ', property='username')
-    global rmq_password
-    rmq_password = af_support_tools.get_config_file_property(config_file=env_file, heading='RabbitMQ', property='password')
-    global rmq_port
-    rmq_port = af_support_tools.get_config_file_property(config_file=env_file, heading='RabbitMQ', property='ssl_port')
-    global rmq_ssl
-    rmq_ssl = True
-    global rmq_cert_path
-    rmq_cert_path = af_support_tools.get_config_file_property(config_file=env_file, heading='RabbitMQ', property='cert_path')
-
-    # Get Rabbit MQ Certs
-    af_support_tools.rmq_get_server_side_certs(host_hostname=cpsd_hostname, host_username=cpsd_username, host_password=cpsd_password, host_port=22, rmq_certs_path=rmq_cert_path)
-
-    # Data Sensitive Properties
     global my_username
     my_username = af_support_tools.get_config_file_property(config_file=config_file, heading='data_sensitive_vars', property='username')
     global my_password
     my_password = af_support_tools.get_config_file_property(config_file=config_file, heading='data_sensitive_vars', property='password')
-    
-@pytest.mark.automation_test_packs
-@pytest.mark.taf_mvp
-def test_sample_fixture(sample_local_fixture, sample_af_fixture):
-    """
-    This sample test is designed to demonstrate py test fixtures.
-    One py test fixture is local to the sample test suite while the other is global to the auto-framework.
-    The point is to show test designers their ability to utilize approved auto-framework fixtures while also maintaining their own fixtures.
-    Fixtures which are deemed universal can be checked into the auto-framework by the automation team.
-    This test is designed to pass.
-    """
-
-    print()
-    print('Username: %s' % my_username)
-    print('Password: %s' % my_password)
-    print()
-
-    # Unpack vars from a local pytest fixture file
-    local_messge_1, local_messge_2, local_messge_3 = sample_local_fixture
-    
-    # Unpack vars from the Automation Framework fixture file
-    af_messge_1, af_messge_2, af_messge_3 = sample_af_fixture
-    
-    # Print out vars
-    print(local_messge_1)
-    print(local_messge_2)
-    print(local_messge_3)
-    print()
-    print(af_messge_1)
-    print(af_messge_2)
-    print(af_messge_3)
-    
-    # Sample 'assert'
-    assert 5 == 5
     
 @pytest.mark.automation_test_packs
 @pytest.mark.taf_mvp
