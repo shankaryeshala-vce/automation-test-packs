d5120b222e574e9674bf92d98496da4848d4a0cb
Amit <Amit.More@emc.com>
2017-07-26 15:17:39 +0530

    changed the script according to new json changes.

--- test-packs/test_suites/continuous-integration-deploy-suite/node-expansion-ci-cd/test_idrac_credential_store.py
+++ test-packs/test_suites/continuous-integration-deploy-suite/node-expansion-ci-cd/test_idrac_credential_store.py
@@ -29,6 +29,22 @@ def load_test_data():
     username = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS', property='username')
     global password
     password = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS', property='password')
+    # Update setup_config.properties file at runtime
+    data_file = os.environ.get('AF_RESOURCES_PATH') + '/continuous-integration-deploy-suite/setup_config.properties'
+    af_support_tools.set_config_file_property_by_data_file(data_file)
+    # IDrac Server IP & Creds details
+    global setup_config_file
+    setup_config_file = 'continuous-integration-deploy-suite/setup_config.ini'
+    global setup_config_header
+    setup_config_header = 'config_details'
+    global idrac_hostname
+    idrac_hostname = af_support_tools.get_config_file_property(config_file=setup_config_file, heading=setup_config_header, property='idrac_ipaddress')
+    global idrac_username
+    idrac_username = af_support_tools.get_config_file_property(config_file=setup_config_file, heading=setup_config_header, property='idrac_username')
+    global idrac_common_password
+    idrac_common_password = af_support_tools.get_config_file_property(config_file=setup_config_file, heading=setup_config_header, property='idrac_common_password')
+    global idrac_factory_password
+    idrac_factory_password = af_support_tools.get_config_file_property(config_file=setup_config_file, heading=setup_config_header, property='idrac_factory_password')
 #####################################################################
 # These are the main tests.
 #####################################################################
@@ -37,8 +53,8 @@ def load_test_data():
 def test_idrac_credential_store():
     """
     Description         :       This method tests whether the idrac credentials are stored in system definition service
-    			it will asserts if :
-    		        If IDRAC json file doesnt exists
+				it will asserts if :
+			        If IDRAC json file doesnt exists
                                 If AMQP tool is not able to execute the command properly
                                 If credential keys for IDRAC are not created properly
     Parameters  	:       None
@@ -49,60 +65,7 @@ def test_idrac_credential_store():
                                                             heading=payload_header,
                                                             property=payload_property_sys)
     container_id = get_vault_container_id()
-    #assert update_IDRAC_json(jsonfilepath, ip_address, the_payload), 'test failed: ' + jsonfilepath + 'doesnt exists'
-    #assert run_amqp_tool(amqptooljar, jsonfilepath), 'test failed: unable to execute ' + amqptooljar
     assert check_vault_credential_keys(container_id),'test failed: idrac credentials are not stored properly'
-def update_IDRAC_json(json_file_path, ipaddress, payload):
-    """
-    Description 	:       This method will update the json file with the host ip address 
-    Parameters  	:       1. json_file_path     -       Name of the Json file (STRING)
-    			2. ipaddress          -  hostname mentioned in the env.ini file
-                                3. payload            - payload for the json file
-    Returns             :       0 or 1 (Boolean)
-    """
-    with open(json_file_path,'w') as outfile:
-        outfile.write(payload)
-    outfile.close()
-    if (os.path.isfile(json_file_path) == 0):
-        return 0
-    with open(json_file_path) as json_file:
-        data = json.load(json_file)
-    data['configuration']['host'] = ipaddress 
-    with open(json_file_path,'w') as outfile:
-        json.dump(data,outfile)
-    return 1
-def get_idrac_credentials_json():
-    """
-    Description 	:       This method will get the idrac credentials from the json file 
-    Parameters  	:       None
-    Returns             :       idrac_credential_json (LIST)
-    """
-    with open(jsonfilepath) as json_file:
-        data = json.load(json_file)
-    idrac_username_json=data['body']['convergedSystem']['endpoints'][3]['credentials'][0]['credentialElements']['username']
-    idrac_password_json=data['body']['convergedSystem']['endpoints'][3]['credentials'][0]['credentialElements']['password']
-    idrac_username_new_json=data['body']['convergedSystem']['endpoints'][3]['credentials'][1]['credentialElements']['username']
-    idrac_password_new_json=data['body']['convergedSystem']['endpoints'][3]['credentials'][1]['credentialElements']['password']
-    idrac_credential_json = [idrac_username_json,idrac_password_json,idrac_username_new_json,idrac_password_new_json]
-    return idrac_credential_json
-def run_amqp_tool(amqp_tool_jar, system_def_json):
-    """
-    Description 	:       This method will run the ampq tool jar file with the given input json file 
-    Parameters  	:       1. amqp_tool_jar       -       Name of the amqp tool jar file (STRING)
-                                2. system_def_json     -       Name of the Json file (STRING)
-    Returns             :       0 or 1 (Boolean)
-    """
-    test_status = "pass"
-    cmd = 'java -jar ' + amqp_tool_jar + ' ' + system_def_json
-    output = os.system(cmd)
-    # Adding sleepp to debug if it is timing issue
-    time.sleep(10)
-    if (output != 0):
-        test_status = "fail"
-    if (test_status == "fail"):
-        return 0
-    else:
-        return 1
 def get_vault_container_id():    
     """    
     Description         :       This method gets the container id    
@@ -124,11 +87,10 @@ def check_vault_credential_keys(container_id):
                                 0 or 1 (Boolean)
     """
     cmd = "docker exec " + container_id + " vault list secret"
-    # Adding print statements to debug
     print ("cmd:",cmd)
     output = af_support_tools.send_ssh_command(host=ip_address, username=username, password=password, command=cmd, return_output=True).strip()    
     output_list = output.split("\n")
-    print ("output_list:",output_list) 
+    print ("output_list:",output_list)
     output_length = len(output_list)
     credential_list_json = ""
     credential_list = 0
@@ -139,32 +101,41 @@ def check_vault_credential_keys(container_id):
     print ("output_list2:",output_list)
     for uuid in output_list:
         cmd1 = "docker exec " + container_id + " vault list secret/" + uuid
+        print ("cmd1:",cmd1)
         output1 = af_support_tools.send_ssh_command(host=ip_address, username=username, password=password, command=cmd1, return_output=True).strip()    
         output_list1 = output1.split("\n")
         endpoint_uuid = output_list1[2]
         cmd2 = "docker exec " + container_id + " vault list secret/" + uuid + endpoint_uuid
+        print ("cmd2;",cmd2)
         output2 = af_support_tools.send_ssh_command(host=ip_address, username=username, password=password, command=cmd2, return_output=True).strip()    
         output_list2 = output2.split("\n")
         output_length2 = len(output_list2)
+        print ("output_length:",output_length2)
         if (output_length2 == 3):
             continue
         else:
+            print ("output_list2:",output_list2)
             idrac_uuid = output_list2[2]
             idrac_uuid_new = output_list2[3]
             cmd3 = "docker exec " + container_id + " vault read secret/" + uuid + endpoint_uuid + idrac_uuid
+            print ("cmd3:",cmd3)
             output3 = af_support_tools.send_ssh_command(host=ip_address, username=username, password=password, command=cmd3, return_output=True).strip()    
-            idrac_username, idrac_passwd = get_idrac_credentials(output3)
+            idrac_usrname, idrac_passwd = get_idrac_credentials(output3)
             cmd4 = "docker exec " + container_id + " vault read secret/" + uuid + endpoint_uuid + idrac_uuid_new
+            print ("cmd4:",cmd4)
             output4 = af_support_tools.send_ssh_command(host=ip_address, username=username, password=password, command=cmd4, return_output=True).strip()    
             idrac_username_new, idrac_passwd_new = get_idrac_credentials(output4)
-            credential_list_json=get_idrac_credentials_json()
-            credential_list=[idrac_username,idrac_passwd,idrac_username_new,idrac_passwd_new]
+            credential_list_json=[idrac_username,idrac_factory_password,idrac_username,idrac_common_password]
+            print ("credential_list_json:",credential_list_json)
+            credential_list=[idrac_usrname,idrac_passwd,idrac_username_new,idrac_passwd_new]
+            print ("credential_list:",credential_list)
+            if credential_list:
+                break
     for element in credential_list_json:
         if (element in credential_list):
              continue
         else:
              return 0 
-    os.remove(jsonfilepath)
     return 1
 def get_idrac_credentials(command_output):
     """
@@ -179,3 +150,4 @@ def get_idrac_credentials(command_output):
     idrac_passwd_list = output_list[4].split("\t")
     idracPasswd = idrac_passwd_list[1]
     return idracUsername, idracPasswd
+
