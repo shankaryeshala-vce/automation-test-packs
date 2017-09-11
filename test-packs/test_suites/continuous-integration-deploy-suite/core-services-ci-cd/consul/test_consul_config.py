6744b411b7de3f24bf393af12535138678537455
Jonathan King <jonathan.king@vce.com>
2017-09-11 16:57:13 +0100

    refactored consul tests (#316)

--- test-packs/test_suites/continuous-integration-deploy-suite/core-services-ci-cd/consul/test_consul_config.py
+++ test-packs/test_suites/continuous-integration-deploy-suite/core-services-ci-cd/consul/test_consul_config.py
@@ -9,51 +9,12 @@
 #
 import pytest
 import requests
-import json
-import time
 import af_support_tools
-import os
 
-
-##############################################################################################
-
-@pytest.fixture(scope="module", autouse=True)
-def load_test_data():
-    # Update config ini files at runtime
-    my_data_file = os.environ.get('AF_RESOURCES_PATH') + '/continuous-integration-deploy-suite/symphony-sds.properties'
-    af_support_tools.set_config_file_property_by_data_file(my_data_file)
-
-    # Set config ini file name
-    global env_file
-    env_file = 'env.ini'
-
-    # Test VM Details
-    global ipaddress
-    ipaddress = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS', property='hostname')
-    global cli_username
-    cli_username = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS',
-                                                             property='username')
-    global cli_password
-    cli_password = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS',
-                                                             property='password')
-
-    # RMQ Details
-    global rmq_username
-    rmq_username = af_support_tools.get_config_file_property(config_file=env_file, heading='RabbitMQ',
-                                                             property='username')
-    global rmq_password
-    rmq_password = af_support_tools.get_config_file_property(config_file=env_file, heading='RabbitMQ',
-                                                             property='password')
-    global port
-    port = af_support_tools.get_config_file_property(config_file=env_file, heading='RabbitMQ',
-                                                     property='ssl_port')
-
-
-##############################################################################################
 
 @pytest.mark.core_services_mvp
 @pytest.mark.core_services_mvp_extended
-def test_consul_servicerunning():
+def test_consul_is_running(hostConnection):
     """
     Test Case Title :       Verify Consul Service is running
     Description     :       This method tests that consul docker container is running
@@ -62,13 +23,14 @@ def test_consul_servicerunning():
     Parameters      :       none
     Returns         :       None
     """
-    print('\n* * * Testing Consul on system:', ipaddress, '* * *\n')
+    print('\n* * * Testing Consul on system:', hostConnection.ipAddress, '* * *\n')
 
     service_name = 'consul'
 
-    sendCommand = "docker ps --filter name=" + service_name + "  --format '{{.Status}}' | awk '{print $1}'"
-    my_return_status = af_support_tools.send_ssh_command(host=ipaddress, username=cli_username, password=cli_password,
-                                                         command=sendCommand, return_output=True)
+    ssh_command = "docker ps --filter name=" + service_name + "  --format '{{.Status}}' | awk '{print $1}'"
+    my_return_status = af_support_tools.send_ssh_command(host=hostConnection.ipAddress, username=hostConnection.username,
+                                                         password=hostConnection.password,
+                                                         command=ssh_command, return_output=True)
     my_return_status = my_return_status.strip()
     print('\nDocker Container is:', my_return_status, '\n')
     assert my_return_status == 'Up', (service_name + " not running")
@@ -76,7 +38,8 @@ def test_consul_servicerunning():
 
 @pytest.mark.core_services_mvp
 @pytest.mark.core_services_mvp_extended
-def test_consul_verify_Consul_registered():
+@pytest.mark.parametrize("service", ["consul", "vault", "api-gateway"])
+def test_service_is_registered_with_consul(hostConnection, service):
     """
     Test Case Title :       Verify Consul is registered with Consul
     Description     :       This method tests that consul is registered in the Consul API http://{SymphonyIP}:8500/v1/agent/services
@@ -87,83 +50,22 @@ def test_consul_verify_Consul_registered():
     Note            :       It's expected that Consul has no Status so there is no Status test.
     """
 
-    service = 'consul'
+    url = 'http://' + hostConnection.ipAddress + ':8500/v1/agent/services'
+    print('sending GET request:', url)
+    response = requests.get(url)
 
-    url_body = ':8500/v1/agent/services'
-    my_url = 'http://' + ipaddress + url_body
+    # Create the string as it should appear in the API
+    service_to_check = '"Service": "' + service + '"'
 
-    print('GET:', my_url)
+    assert response.status_code == 200
+    assert service_to_check in response.text, ('ERROR:', service, 'is not in Consul\n')
 
-    try:
-        url_response = requests.get(my_url)
-        url_response.raise_for_status()
-
-        # A 200 has been received
-        print(url_response)
-
-        the_response = url_response.text
-
-        # Create the sting as it should appear in the API
-        serviceToCheck = '"Service": "' + service + '"'
-
-        assert serviceToCheck in the_response, ('ERROR:', service, 'is not in Consul\n')
-
-        print(service, 'Registered in Consul')
-
-    # Error check the response
-    except Exception as err:
-        # Return code error (e.g. 404, 501, ...)
-        print(err)
-        print('\n')
-        raise Exception(err)
 
 
 @pytest.mark.core_services_mvp
 @pytest.mark.core_services_mvp_extended
-def test_consul_verify_Vault_registered():
-    """
-    Test Case Title :       Verify Vault is registered with Consul
-    Description     :       This method tests that vault is registered in the Consul API http://{SymphonyIP}:8500/v1/agent/services
-                            It will fail if :
-                                The line 'Service: "vault"' is not present
-    Parameters      :       none
-    Returns         :       None
-    """
-
-    service = 'vault'
-
-    url_body = ':8500/v1/agent/services'
-    my_url = 'http://' + ipaddress + url_body
-
-    print('GET:', my_url)
-
-    try:
-        url_response = requests.get(my_url)
-        url_response.raise_for_status()
-
-        # A 200 has been received
-        print(url_response)
-
-        the_response = url_response.text
-
-        # Create the sting as it should appear in the API
-        serviceToCheck = '"Service": "' + service + '"'
-
-        assert serviceToCheck in the_response, ('ERROR:', service, 'is not in Consul\n')
-
-        print(service, 'Registered in Consul')
-
-    # Error check the response
-    except Exception as err:
-        # Return code error (e.g. 404, 501, ...)
-        print(err)
-        print('\n')
-        raise Exception(err)
-
-
-@pytest.mark.core_services_mvp
-@pytest.mark.core_services_mvp_extended
-def test_consul_verify_Vault_status():
+@pytest.mark.parametrize("service", ["vault", "api-gateway"])
+def test_service_status_is_healthy_in_consul(hostConnection, service):
     """
     Test Case Title :       Verify Vault is Passing in Consul
     Description     :       This method tests that vault has a passing status in the Consul API http://{SymphonyIP}:8500/v1/health/checks/vault
@@ -172,110 +74,12 @@ def test_consul_verify_Vault_status():
     Parameters      :       none
     Returns         :       None
     """
-    service = 'vault'
 
-    url_body = ':8500/v1/health/checks/' + service
-    my_url = 'http://' + ipaddress + url_body
+    url = 'http://' + hostConnection.ipAddress + ':8500/v1/health/checks/' + service
+    print('GET:', url)
 
-    print('GET:', my_url)
+    response = requests.get(url)
+    service_status = '"Status": "passing"'
 
-    try:
-        url_response = requests.get(my_url)
-        url_response.raise_for_status()
-
-        # A 200 has been received
-        print(url_response)
-        the_response = url_response.text
-
-        serviceStatus = '"Status": "passing"'
-        assert serviceStatus in the_response, ('ERROR:', service, 'is not Passing in Consul\n')
-        print(service, 'Status = Passing in consul\n\n')
-
-    # Error check the response
-    except Exception as err:
-        # Return code error (e.g. 404, 501, ...)
-        print(err)
-        print('\n')
-        raise Exception(err)
-
-
-@pytest.mark.core_services_mvp
-@pytest.mark.core_services_mvp_extended
-def test_consul_verify_api_gateway_registered():
-    """
-    Test Case Title :       Verify api-gateway is registered with Consul
-    Description     :       This method tests that api-gateway is registered in the Consul API http://{SymphonyIP}:8500/v1/agent/services
-                            It will fail if :
-                                The line 'Service: "api-gateway"' is not present
-    Parameters      :       none
-    Returns         :       None
-    """
-
-    service = 'api-gateway'
-
-    url_body = ':8500/v1/agent/services'
-    my_url = 'http://' + ipaddress + url_body
-
-    print('GET:', my_url)
-
-    try:
-        url_response = requests.get(my_url)
-        url_response.raise_for_status()
-
-        # A 200 has been received
-        print(url_response)
-
-        the_response = url_response.text
-
-        # Create the sting as it should appear in the API
-        serviceToCheck = '"Service": "' + service + '"'
-
-        assert serviceToCheck in the_response, ('ERROR:', service, 'is not in Consul\n')
-
-        print(service, 'Registered in Consul')
-
-    # Error check the response
-    except Exception as err:
-        # Return code error (e.g. 404, 501, ...)
-        print(err)
-        print('\n')
-        raise Exception(err)
-
-
-@pytest.mark.core_services_mvp
-@pytest.mark.core_services_mvp_extended
-def test_consul_verify_api_gateway_status():
-    """
-    Test Case Title :       Verify api-gateway is Passing in Consul
-    Description     :       This method tests that api-gateway has a passing status in the Consul API http://{SymphonyIP}:8500/v1/health/checks/api-gateway
-                            It will fail if :
-                                The line '"Status": "passing"' is not present
-    Parameters      :       none
-    Returns         :       None
-    """
-    service = 'api-gateway'
-
-    url_body = ':8500/v1/health/checks/' + service
-    my_url = 'http://' + ipaddress + url_body
-
-    print('GET:', my_url)
-
-    try:
-        url_response = requests.get(my_url)
-        url_response.raise_for_status()
-
-        # A 200 has been received
-        print(url_response)
-        the_response = url_response.text
-
-        serviceStatus = '"Status": "passing"'
-        assert serviceStatus in the_response, ('ERROR:', service, 'is not Passing in Consul\n')
-        print(service, 'Status = Passing in consul\n\n')
-
-    except Exception as err:
-        # Return code error (e.g. 404, 501, ...)
-        print(err)
-        print('\n')
-        raise Exception(err)
-
-##############################################################################################
+    assert response.status_code == 200
+    assert service_status in response.text, ('ERROR:', service, 'is not Passing in Consul\n')
