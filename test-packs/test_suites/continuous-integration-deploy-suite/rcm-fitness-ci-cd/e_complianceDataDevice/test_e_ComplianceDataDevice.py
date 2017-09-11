6510722cba1b6f52dd347acca5a061c54f3cbf0f
hennet1 <trevor.hennessy@dell.com>
2017-09-11 17:04:13 +0100

    increase wait after requesting

--- test-packs/test_suites/continuous-integration-deploy-suite/rcm-fitness-ci-cd/e_complianceDataDevice/test_e_ComplianceDataDevice.py
+++ test-packs/test_suites/continuous-integration-deploy-suite/rcm-fitness-ci-cd/e_complianceDataDevice/test_e_ComplianceDataDevice.py
@@ -30,7 +30,7 @@ def load_test_data():
     purgeOldOutput(path, "complianceDataDevice")
 
     initUrl = 'http://' + host + ':10000/rcm-fitness-paqx/rcm-fitness-api/api/about'
-    time.sleep(2)
+    time.sleep(15)
     getSystemDefinition()
 
 
