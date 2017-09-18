e765a97efdb74ca4883c9f8d31093ffd23e63087
akhtat1 <toqeer.akhtar@vce.com>
2017-09-18 12:44:08 +0100

    Update test_d_convergedsystems_api_CI_CD.py

--- test-packs/test_suites/continuous-integration-deploy-suite/core-services-ci-cd/c_SystenDefinition/test_d_convergedsystems_api_CI_CD.py
+++ test-packs/test_suites/continuous-integration-deploy-suite/core-services-ci-cd/c_SystenDefinition/test_d_convergedsystems_api_CI_CD.py
@@ -19,7 +19,6 @@ import os
 
 @pytest.fixture(scope="module", autouse=True)
 def load_test_data():
-
     # Update config ini files at runtime
     my_data_file = os.environ.get('AF_RESOURCES_PATH') + '/continuous-integration-deploy-suite/symphony-sds.properties'
     af_support_tools.set_config_file_property_by_data_file(my_data_file)
@@ -46,10 +45,11 @@ def load_test_data():
     cli_password = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS',
                                                              property='password')
 
+
 @pytest.mark.core_services_mvp
 @pytest.mark.core_services_mvp_extended
 def test_ConvergedSystem_RestAPI():
-    test_errors=[]
+    test_errors = []
 
     """ Verify the Converged System Components Rest API V's JSON input file.
 
@@ -57,73 +57,73 @@ def test_ConvergedSystem_RestAPI():
                     You can open a browser to the Converged System Rest API (e.g. http://10.20.30.40:8088/convergedsystems)
     """
 
-# Check the JSON system definition input file
-    the_payload = af_support_tools.get_config_file_property(config_file=payload_file, heading= payload_header,
-                                                            property= payload_property_sysadd )
+    # Check the JSON system definition input file
+    the_payload = af_support_tools.get_config_file_property(config_file=payload_file, heading=payload_header,
+                                                            property=payload_property_sysadd)
     sysadd = json.loads(the_payload)
 
-# =================================================================
-# Converged System Rest API
+    # =================================================================
+    # Converged System Rest API
 
-    RestAPIurl = 'http://'+ ipaddress +':8088/convergedsystems'
+    RestAPIurl = 'http://' + ipaddress + ':8088/convergedsystems'
     resp = requests.get(RestAPIurl)
     Rest_data = json.loads(resp.text)
     time.sleep(5)
-    print (Rest_data)
+    print(Rest_data)
 
-# Find the Converged Systems UUID to build the Converged Systems Components Rest API
-    # Rest_Component_element = Rest_data[0]["uuid"]
-    # 
-    # RestAPI_Component_url = u'http://' + ipaddress + u':8088/convergedsystems/' + Rest_Component_element + u'/components'
-    # comp_resp = requests.get(RestAPI_Component_url)
-    # Rest_Component_data = json.loads(comp_resp.text)
-    # 
-    # # Check Common UI - System Definition
-    # 
-    # 
-    # if Rest_data[0]["identity"]["identifier"] != sysadd["body"]["convergedSystem"]["identity"]["identifier"]:
-    #     test_errors.append("Error---System types / identifier not matching")
-    # assert not test_errors
-    # 
-    # if Rest_data[0]["identity"]["serialNumber"] != sysadd["body"]["convergedSystem"]["identity"]["serialNumber"]:
-    #     test_errors.append("Error---Serial numbers not matching")
-    # assert not test_errors
-    # 
-    # if Rest_data[0]["definition"]["productFamily"] != sysadd["body"]["convergedSystem"]["definition"]["productFamily"]:
-    #     test_errors.append("Error---Product Family not matching")
-    # assert not test_errors
-    # 
-    # if Rest_data[0]["definition"]["product"] != sysadd["body"]["convergedSystem"]["definition"]["product"]:
-    #     test_errors.append("Error---Product not matching")
-    # assert not test_errors
-    # 
-    # if Rest_data[0]["definition"]["model"] != sysadd["body"]["convergedSystem"]["definition"]["model"]:
-    #     test_errors.append("Error---Model not matching")
-    # assert not test_errors
-    # 
-    # # Check Common UI - System Components - Row 0
-    # 
-    # if Rest_Component_data[0]["identity"]["elementType"] != \
-    #         sysadd["body"]["convergedSystem"]["components"][0]["identity"]["elementType"]:
-    #     test_errors.append("Error---Component Type not matching")
-    # assert not test_errors
-    
+    # Find the Converged Systems UUID to build the Converged Systems Components Rest API
+    #Rest_Component_element = Rest_data[0]["uuid"]
+
+    RestAPI_Component_url = u'http://' + ipaddress + u':8088/convergedsystems/' + Rest_data[0]["uuid"] + u'/components'
+    comp_resp = requests.get(RestAPI_Component_url)
+    Rest_Component_data = json.loads(comp_resp.text)
+
+    # Check Common UI - System Definition
+
+
+    if Rest_data[0]["identity"]["identifier"] != sysadd["body"]["convergedSystem"]["identity"]["identifier"]:
+        test_errors.append("Error---System types / identifier not matching")
+    assert not test_errors
+
+    if Rest_data[0]["identity"]["serialNumber"] != sysadd["body"]["convergedSystem"]["identity"]["serialNumber"]:
+        test_errors.append("Error---Serial numbers not matching")
+    assert not test_errors
+
+    if Rest_data[0]["definition"]["productFamily"] != sysadd["body"]["convergedSystem"]["definition"]["productFamily"]:
+        test_errors.append("Error---Product Family not matching")
+    assert not test_errors
+
+    if Rest_data[0]["definition"]["product"] != sysadd["body"]["convergedSystem"]["definition"]["product"]:
+        test_errors.append("Error---Product not matching")
+    assert not test_errors
+
+    if Rest_data[0]["definition"]["model"] != sysadd["body"]["convergedSystem"]["definition"]["model"]:
+        test_errors.append("Error---Model not matching")
+    assert not test_errors
+
+    # Check Common UI - System Components - Row 0
+
+    if Rest_Component_data[0]["identity"]["elementType"] != \
+            sysadd["body"]["convergedSystem"]["components"][0]["identity"]["elementType"]:
+        test_errors.append("Error---Component Type not matching")
+    assert not test_errors
+
     ## ---TA --- Disabling following checks as indexing can be different each time system is added
 
     # if Rest_Component_data[0]["identity"]["identifier"] != sysadd["body"]["convergedSystem"]["components"][2]["identity"]["identifier"]:
     #     test_errors.append("Error---Component Name / identifier not matching")
     # assert not test_errors
-    # 
+    #
     # if Rest_Component_data[0]["definition"]["productFamily"] != sysadd["body"]["convergedSystem"]["components"][0]["definition"]["productFamily"]:
     #     test_errors.append("Error---Component Product family not matching")
     # assert not test_errors
-    # 
+    #
     # if Rest_Component_data[0]["definition"]["modelFamily"]!= sysadd["body"]["convergedSystem"]["components"][0]["definition"]["modelFamily"]:
     #     test_errors.append("Error---Component Model family not matching")
     # assert not test_errors
-    # 
+    #
     # if  Rest_Component_data[0]["definition"]["model"]!= sysadd["body"]["convergedSystem"]["components"][0]["definition"]["model"]:
     #     test_errors.append("Error---Component Model not matching")
     # assert not test_errors
 
-# ---------------------------------------------------------------------
+    # ---------------------------------------------------------------------
