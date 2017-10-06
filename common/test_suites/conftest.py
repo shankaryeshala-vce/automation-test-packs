3f3de111eadc64d11d3235118680ae499105cfed
hurlec3 <catherine.hurley@dell.com>
2017-10-06 11:15:45 +0000

    changed to communicate

--- common/test_suites/conftest.py
+++ common/test_suites/conftest.py
@@ -31,6 +31,8 @@ def get_tls_certs():
         print('TLS Certs exist already')
     else:
         ex = subprocess.Popen('chmod +x tls-enable.sh', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).wait()
-        out = subprocess.check_output('./tls-enable.sh')
+        p = subprocess.Popen('./tls-enable.sh', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        p.wait()
+        out, err = p.communicate()
         with open(tls_file,'w') as f:
             f.write(out)
