990e28247e737bcd026f8ced907271ffdf10a4fc
hurlec3 <catherine.hurley@dell.com>
2017-10-06 10:52:06 +0000

    added set -x

--- common/test_suites/tls_enable.sh
+++ common/test_suites/tls_enable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 curl --insecure https://tls-service.cpsd.dell:7001/v1/pki/ca/ | sudo tee /usr/local/share/ca-certificates/cpsd.dell.ca.crt
 sudo update-ca-certificates
 curl -O --header "Content-Type: application/json" --request POST --data '{"common_name": "taf.cpsd.dell"}' https://tls-service.cpsd.dell:7001/v1/pki/issue/bundle.tar.gz
