b477f042af8ecd53c379537128bc38fa333a6a99
hurlec3 <catherine.hurley@dell.com>
2017-10-08 22:38:50 +0000

    Added Test for RMQ credentials

--- test-packs/test_suites/tls_enabled/test_rmq_creds.py
+++ test-packs/test_suites/tls_enabled/test_rmq_creds.py
@@ -1,6 +1,11 @@
 from common_libs import cpsd
+import pytest
 
-
+@pytest.mark.tls_test
 def test_rmq_creds():
-    creds = cpsd.props.get_rmq_creds()
-    print(creds)
+    print("About to get creds....")
+    creds = cpsd.get_rmq_credentials()
+    print("RabbitMQ User is ", creds['rmq_user'])
+    print("RabbitMQ Password is", creds['rmq_password'])
+    assert creds['rmq_user'], "rmq user not returned"
+    assert creds['rmq_password'], "rmq password not returned"
