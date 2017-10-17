41cb43864ee966f3cc09562e73bf4413886061fb
whoolt <Ted.Whooley@vce.com>
2017-10-17 10:15:05 +0000

    Adding new marker tls_enabled_stop_start

--- test-packs/run_jobs/run_tls_enabled_test_suite.sh
+++ test-packs/run_jobs/run_tls_enabled_test_suite.sh
@@ -7,4 +7,5 @@ MARKER='tls_enabled'
 py3clean .
 export AF_TEST_SUITE_NAME=$TEST_SUITE_NAME
 py.test $SEARCH_PATH -m "$MARKER" --html $AF_REPORTS_PATH/all/$REPORT_NAME.html --self-contained-html --json $AF_REPORTS_PATH/all/$REPORT_NAME.json --junit-xml $AF_REPORTS_PATH/all/$REPORT_NAME.xml
+py.test $SEARCH_PATH -m "tls_enabled_stop_start" --html $AF_REPORTS_PATH/all/$REPORT_NAME.html --self-contained-html --json $AF_REPORTS_PATH/all/$REPORT_NAME.json --junit-xml $AF_REPORTS_PATH/all/$REPORT_NAME.xml
 export AF_TEST_SUITE_NAME='Test Suite Name Not Set'
