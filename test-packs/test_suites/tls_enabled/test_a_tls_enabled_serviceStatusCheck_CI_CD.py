1b8b51ef6bca9f8632541aaf11a7e061d8442b56
whoolt <Ted.Whooley@vce.com>
2017-10-17 08:49:24 +0000

    Temporarily disabling stop/start tests

--- test-packs/test_suites/tls_enabled/test_a_tls_enabled_serviceStatusCheck_CI_CD.py
+++ test-packs/test_suites/tls_enabled/test_a_tls_enabled_serviceStatusCheck_CI_CD.py
@@ -114,7 +114,7 @@ def test_coreamqptls(core_services, setup):
     assert not err
 
 
-@pytest.mark.tls_enabled
+#@pytest.mark.tls_enabled
 @pytest.mark.core_services_mvp_extended
 def test_core_stop(core_container, setup):
     """
@@ -147,7 +147,7 @@ def test_core_stop(core_container, setup):
     assert "Exited" in response2, "%s did not stop or container has been removed" % core_container
 
 
-@pytest.mark.tls_enabled
+#@pytest.mark.tls_enabled
 @pytest.mark.core_services_mvp_extended
 def test_core_start(core_container, setup):
     """
