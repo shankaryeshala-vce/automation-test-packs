801f549d19379f9d28b214e49d9a5918e9539f52
akhtat1 <toqeer.akhtar@vce.com>
2017-10-19 15:51:37 +0100

    adding sds Consul tests and tls_enabled markers

--- test-packs/test_suites/continuous-integration-deploy-suite/core-services-ci-cd/b_SystemAddRequest/test_b_amqp_api_post.py
+++ test-packs/test_suites/continuous-integration-deploy-suite/core-services-ci-cd/b_SystemAddRequest/test_b_amqp_api_post.py
@@ -33,6 +33,7 @@ def load_test_data():
     cli_password = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS',
                                                              property='password')
 
+@pytest.mark.tls_enabled
 @pytest.mark.core_services_mvp
 @pytest.mark.core_services_mvp_extended
 def test_install_amqpapi():
@@ -51,7 +52,7 @@ def test_install_amqpapi():
         err.append(amqpapi+ " did not install properly")
     assert not err
 
-
+@pytest.mark.tls_enabled
 @pytest.mark.core_services_mvp
 @pytest.mark.core_services_mvp_extended
 def test_api_addsystem():
@@ -87,6 +88,32 @@ def test_api_addsystem():
                     sysadd["body"]["convergedSystem"]["components"][3] != resp["body"]["convergedSystem"]["components"][3]):
 
         err.append("Error---All Components are not added successfully")
+    assert not err
+    
+@pytest.mark.tls_enabled
+def test_sds_consul():
+    """
+            Title: sds Registered in con
+            Description: This test verify SystemDefinition Services is registered with consul
+            Params: None
+            Returns: None
+        """
+    print(test_sds_consul.__doc__)
+    err = []
+
+
+    consul_url = 'https://' + consulHost + ':8500/v1/catalog/services'
+    resp = requests.get(consul_url, verify= '/usr/local/share/ca-certificates/taf.cpsd.dell.ca.crt')
+    data = json.loads(resp.text)
+
+    assert resp.status_code == 200, "Request has not been acknowledged as expected."
+
+    if 'system-definition-service' not in data:
+        err.append("Error--- system-definition-service not registered in Consul")
+    assert not err
+
+    if 'postgres' not in data:
+        err.append("Error--- Postgres not registered in Consul")
     assert not err
 
 
