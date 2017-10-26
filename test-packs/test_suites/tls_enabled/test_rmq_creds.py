a5d466aed070ab82590538b44094ef9d581a448f
akhtat1 <toqeer.akhtar@vce.com>
2017-10-26 16:52:57 +0100

    removed rmq password
    RMQ function is not returning password

--- test-packs/test_suites/tls_enabled/test_rmq_creds.py
+++ test-packs/test_suites/tls_enabled/test_rmq_creds.py
@@ -5,7 +5,6 @@ import pytest
 def test_rmq_creds():
     print("About to get creds....")
     creds = cpsd.get_rmq_credentials()
-    print("RabbitMQ User is ", creds['rmq_user'])
-    print("RabbitMQ Password is", creds['rmq_password'])
+    print("RabbitMQ User is ", creds['rmq_user'])   
     assert creds['rmq_user'], "rmq user not returned"
-    assert creds['rmq_password'], "rmq password not returned"
+    
