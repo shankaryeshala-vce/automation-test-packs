669757b28d23f13130eb6e5fcd0d4f92f33b4791
hurlec3 <catherine.hurley@dell.com>
2017-11-06 15:58:46 +0000

    Added request to pam service to create user for taf

--- common/test_suites/conftest.py
+++ common/test_suites/conftest.py
@@ -3,11 +3,9 @@ import af_support_tools
 import os
 import pytest
 import subprocess
-# from common.common_libs import cpsd
-#
-#
-# passwords = cpsd.get_rmq_credentials()
-# print(passwords)
+import requests
+import json
+import socket
 
 
 @pytest.fixture(autouse=True, scope='session')
@@ -25,19 +23,28 @@ def cpsd_common_properties():
 
 @pytest.fixture(autouse=True, scope='session')
 def get_tls_certs():
+    hostname = socket.gethostname()
     print('Getting tls certs from tls_service')
-    tls_file = '/home/autouser/PycharmProjects/auto-framework/test_suites/tls_certs_exist'
+    tls_file = '/usr/local/share/ca-certificates/' + hostname + '.cpsd.dell.ca.crt'
     if os.path.isfile(tls_file):
         print('TLS Certs exist already')
     else:
         ex = subprocess.Popen('chmod +x tls_enable.sh', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         ex = ex.wait()
         o = subprocess.check_output('ls')
-        with open(tls_file,'w') as f:
-            f.write(o.decode("utf-8") )
-        print (o)
+        with open(tls_file, 'w') as f:
+            f.write(o.decode("utf-8"))
+        print(o)
         p = subprocess.check_output('./test_suites/tls_enable.sh')
         # p = subprocess.Popen('./tls-enable.sh', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         # p.wait()
         # out, err = p.communicate()
         print(p)
+    print('Creating test user in Rabbitmq')
+    r = requests.put("https://pam-service.cpsd.dell:7002/pam-service/v1/amqp/users", cert=(
+        '/usr/local/share/ca-certificates/' + hostname + '.cpsd.dell.crt',
+        '/usr/local/share/ca-certificates/' + hostname + '.cpsd.dell.key'),
+                     verify='/usr/local/share/ca-certificates/cpsd.dell.ca.crt')
+
+    assert r.status_code == 200, "Error---Rabbitmq credentials for test not created"
+
